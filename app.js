54ad52130245b49c104bd172a1153a4c719fa2ab
TabethaSnead <tabetha.snead@gmail.com>
2023-01-18 12:16:28 -0800

    got elements from DOM in javascript, added initial goblin array

--- app.js
+++ app.js
@@ -1,8 +1,30 @@
 /* Imports */
 
 /* Get DOM Elements */
+const defeatedGoblin = document.getElementById('defeated-goblin');
+const goblinInput = document.getElementById('goblin-input');
+const goblinButton = document.getElementById('goblin-button');
+const addedGoblin = document.getElementById('added-goblin');
+const gandalfHPel = document.getElementById('gandalf-hp-span');
+const gandalfimg = document.getElementById('gandalf-img');
 
 /* State */
+let gandalfsHP = 20;
+let defeatCount = 0;
+const goblins = [
+    {
+        name: 'Azog',
+        hp: 11,
+    },
+    {
+        name: 'Bolg',
+        hp: 13,
+    },
+    {
+        name: 'Golfimbul',
+        hp: 10,
+    },
+];
 
 /* Events */
 
